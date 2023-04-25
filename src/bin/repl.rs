04a55b734df4b97b0f6a04243c17a520f2d3811c
that-ambuj <ambujs89@gmail.com>
2023-04-25 15:52:19 +0530

    add repl binary using statements and commands

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -1,42 +1,63 @@
-use std::io::{self, BufWriter, Write};
+use anyhow::{anyhow, Result};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
+
+use simple_sqlite_clone::Statement;
 
 #[derive(Debug)]
 pub enum ExitCode {
-    // OK,
+    OK,
     Failure,
     EOF,
     Error,
 }
 
-impl From<std::io::Error> for ExitCode {
-    fn from(_: std::io::Error) -> Self {
-        ExitCode::Error
-    }
-}
-
-pub fn run() -> Result<(), ExitCode> {
-    let mut stdout = BufWriter::new(io::stdout());
+pub fn run() -> Result<()> {
+    let mut stdout = io::stdout();
     stdout.write_all(b"db -> ")?;
     stdout.flush()?;
 
     let mut input = String::new();
 
     if io::stdin().read_line(&mut input).unwrap() > 0 {
-        match input.as_ref() {
-            ".exit\n" => {
-                println!("Finished");
-                return Ok(());
-            }
-            _ => {
-                eprintln!("Unrecognized command: {}", input);
-                return Err(ExitCode::Failure);
+        if let (".", _) = input.split_at(1) {
+            if let Err(e) = run_meta_command(&input) {
+                eprintln!("{}", e);
+                exit(ExitCode::Failure as i32)
+            } else {
+                println!("Executed.");
             }
         }
+
+        let statement = Statement::prepare(&input)?;
+        statement.execute()?;
+
+        Ok(())
     } else {
-        return Err(ExitCode::EOF);
-    };
+        exit(ExitCode::EOF as i32);
+    }
+}
+
+/// Meta Commands are the commands that don't actually
+/// change any data in the database but are used for repl specific
+/// interactions. They are basically Non-SQL commands.
+/// For Example `.exit`
+fn run_meta_command(cmd: &str) -> Result<()> {
+    match cmd {
+        ".exit\n" => {
+            println!("Finished");
+            exit(ExitCode::OK as i32)
+        }
+        _ => {
+            return Err(anyhow!("Unrecognised command: {}", cmd));
+        }
+    }
 }
 
 fn main() {
-    run().unwrap();
+    loop {
+        let _ = run();
+    }
 }
